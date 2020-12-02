cf56922ec24a2dd837994c6e40a6a91fcc3e58d9
juan-guarin <65561271+juan-guarin@users.noreply.github.com>
2020-12-02 12:29:47 -0500

    perro hp

--- src/PPI/Modelos/ModeloSesion.java
+++ src/PPI/Modelos/ModeloSesion.java
@@ -11,7 +11,7 @@ public class ModeloSesion {
     int tipoUsuario;
     String Nick;
     String Correo;
-    String Puto;
+    String gonorrea;
 
     public boolean isSesionActiva() {
         return sesionActiva;
